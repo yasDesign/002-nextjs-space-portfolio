74923ddbce3ffd824a93d2996d8c5a67dd10051a
yasDesign <ingyasmani@gmail.com>
2022-09-03 19:41:23 -0400

    add img

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="assets/css/slick.css" type="text/css" /> 
+      </Head>
+      <body>
+        <Main />
+        <NextScript/>
+            <Script src="assets/js/jquery-3.5.1.min.js" strategy='beforeInteractive'></Script>
+            <Script src="assets/js/bootstrap.min.js" strategy='beforeInteractive'></Script>
+            <Script src="assets/js/slick.js" strategy='beforeInteractive'></Script>
+            <Script src="assets/js/templatemo-script.js" strategy='beforeInteractive'></Script>
+
+
+      </body>
+    </Html>
+  )
+}
